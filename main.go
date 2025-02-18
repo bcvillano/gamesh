ab2cb04eb210d8e84fe2a46e8c6df854c38b3eb5
bvillano05 <bvillano05@gmail.com>
2025-02-18 10:12:17 -0500

    Basic structure of game

--- main.go
+++ main.go
@@ -2,12 +2,107 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
 )
 
+var wordbank = []string{"redteam", "shell", "service", "downtime"}
+var hangmanStages = []string{
+	`
+         -----
+         |   |
+             |
+             |
+             |
+             |
+        =======
+        `,
+	`
+         -----
+         |   |
+         O   |
+             |
+             |
+             |
+        =======
+        `,
+	`
+         -----
+         |   |
+         O   |
+         |   |
+             |
+             |
+        =======
+        `,
+	`
+         -----
+         |   |
+         O   |
+        /|   |
+             |
+             |
+        =======
+        `,
+	`
+         -----
+         |   |
+         O   |
+        /|\  |
+             |
+             |
+        =======
+        `,
+	`
+         -----
+         |   |
+         O   |
+        /|\  |
+        /    |
+             |
+        =======
+        `,
+	`
+         -----
+         |   |
+         O   |
+        /|\  |
+        / \  |
+        =======
+        `,
+}
+
+func playHangman() bool {
+	length := len(hangmanStages)
+	word := wordbank[rand.Intn(len(wordbank))]
+	var userin string
+	for i := 0; i < length-1; i++ {
+		fmt.Println(hangmanStages[i])
+		fmt.Print("\nGuess: ")
+		_, err := fmt.Scan(&userin)
+		if err != nil {
+			println(err.Error())
+			return false
+		}
+		fmt.Println("User Input: " + userin)
+		if userin == word {
+			return true
+		}
+	}
+	fmt.Println(hangmanStages[length-1])
+	fmt.Println("You lose! Try again...\n\n")
+	return false
+}
+
 func main() {
-	fmt.Println("Opening bash...")
+	for {
+		gameWon := playHangman()
+		if gameWon {
+			break
+		}
+	}
+	fmt.Println("Launching bash...")
 	cmd := exec.Command("/bin/bash")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
